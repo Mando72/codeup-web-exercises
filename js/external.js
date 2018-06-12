7c63999eea41d566f2982574d1cd4cee7d982922
Armando Sifuentes <armandosifuentes@Armandos-MacBook-Air.local>
2018-06-12 15:36:08 -0500

    javascript exercises

---
+++ js/external.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var message  = 'Hello From Javascript';
+
+console.log('Hello From Javascript');
+
+alert("Welcome to my Website!");
+
+var userInput = prompt('What is your favorite color?:');
+console.log('Blue is my favorite color.: ' + userInput);
+
+alert("Blue is also my favorite color also.");
+
+var googleHourlyRateDollars = prompt('What is your hourly rate at google?:');
+alert('400');
+
+var amazonHourlyRateDollars = prompt('What is your hourly rate at amazon?:');
+alert('380');
+var facebookHourlyRateDollars = prompt('What is your hourly rate at facebook?:');
+alert('350');
+
+
+var googleHours = prompt('How many hours did you work at google?:');
+alert('6');
+var amazonHours = prompt('How many hours did you work at amazon?:');
+alert('4');
+var facebookHours = prompt('How many hours did you work facebook?:');
+alert('10');
+var totalPayment = prompt('What is your total payment?:');
+alert('$7,420');
+
+
