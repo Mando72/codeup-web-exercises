1051c4c0d91f443244904abba1790c08a5c6d042
Armando Sifuentes <armandosifuentes@Armandos-Air.attlocal.net>
2018-06-17 23:38:11 -0500

    practice coding

---
+++ js/CountToTen.js
@@ -0,0 +1,10 @@
+"use Strict";
+
+function countToTen(){
+    var count = 0;
+    while (count < 10) {
+        count++;
+        document.getElementById("theCount").innerHTML +=
+            count + "<br>";
+    }
+}
