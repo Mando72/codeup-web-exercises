94c4a46003e5b7d279996e032ee5c1f90ba1f133
Armando Sifuentes <armandosifuentes@Armandos-MacBook-Air.local>
2018-06-18 14:43:10 -0500

    work in progress

---
+++ js/do_while_loop.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+var i = 50;
+
+do {
+
+    console.log(Math.floor(Math.random() * 50) + 50);
+
+    console.log("cones to sell.");
+
+}
+while (i > 50 && i < 100);
