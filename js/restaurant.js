a32e65b1513a9090e721d14b09ce448268e8ceb2
Armando Sifuentes <armandosifuentes@Armandos-MacBook-Air.local>
2018-07-02 13:52:02 -0500

    working on warm up problems for practice

---
+++ js/restaurant.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var restaurants = [
+    {
+        Name: "Tink-a-Taco",
+        City: "San Antonio",
+        State: "Tx",
+        Category: "Tex-Mex",
+        Stars: "5 stars"
+
+    }
+
+];
+
