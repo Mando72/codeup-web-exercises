dc3c0190df2632b16fd26096349c5f5cdfcf78da
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-07-09 09:11:14 -0500

    completed #3 on warm-up practice

--- js/restaurant.js
+++ js/restaurant.js
@@ -6,9 +6,39 @@ var restaurants = [
         City: "San Antonio",
         State: "Tx",
         Category: "Tex-Mex",
-        Stars: "5 stars"
-
+        Stars: "4"
+    },
+    {
+        Name: "Goombas Pizza",
+        City: "San Antonio",
+        State: "Tx",
+        Category: "Italian",
+        Stars: "4"
+    },
+    {
+        Name: "Golden Wok",
+        City: "San Antonio",
+        State: "Tx",
+        Category: "Chinese",
+        Stars: "5"
     }
 
 ];
+
+restaurants.forEach(function(restaurant){
+    console.log(restaurant.Name  + " is a " + restaurant.Category  +
+        " in " + restaurant.City  + restaurant.State + ". " +
+        "I would give it a " + restaurant.Stars +  " star rating" );
+});
+
+var nameLocation = document.getElementsByTagName('ul');
+
+restaurants.forEach(function(nameLocation){
+    console.log(nameLocation);
+});
+
+var index = restaurants.lastIndexOf();
+console.log(index);
+
+
 
