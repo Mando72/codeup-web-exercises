27f55e64c72c0f30134bb61246b62194e18d8c28
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-07-13 13:47:50 -0500

    completed the problems

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,45 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+let lang = users.filter(user => user.languages.length > 2);
+console.log('lang', lang);
+
+let mail = users.map(user => user.email);
+console.log('mail', mail);
+
+let newObject = users.reduce((obj, array) => {
+    array[obj.id] = obj;
+    return array;
+}, {});
+console.log(newObject);
+
